e12c4bcc862369fa3517018068dc2a94cb9ad40a
Manabu Ori <orim@jp.ibm.com>
2011-03-16 03:53:56 +0000

    handle multiple URLs

--- bin/mirror.sh
+++ bin/mirror.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-topdir=/home/ori/tepco
+export TZ=Japan
 
+topdir=`pwd`
 logdir=${topdir}/logs
 date=`date '+%Y%m%d-%H%M'`
 
-#(cd ${topdir} && ./bin/remote_sync.sh > ${logdir}/log.remote.${date} 2>&1)
-(cd ${topdir} && ./bin/pavuk.sh > ${logdir}/log.pavuk.${date} 2>&1)
+cat ${topdir}/mirror.conf | while read line; do
+	echo ${line} | grep '^#' > /dev/null 2>&1
+	if [ $? = 0 ]; then
+		echo "comment: ${line}"
+	else
+		echo "url: ${line}"
+		echo "url: ${line}" >> ${logdir}/log.pavuk.${date}
+		(cd ${topdir} && ./bin/pavuk.sh ${line} >> ${logdir}/log.pavuk.${date} 2>&1)
+	fi
+done
 (cd ${topdir} && ./bin/local_sync.sh > ${logdir}/log.local.${date} 2>&1)
 
