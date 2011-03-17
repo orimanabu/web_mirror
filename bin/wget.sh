9f407c0bb3648fb460219163a82a0256caa58cf4
Manabu Ori <orim@jp.ibm.com>
2011-03-17 15:42:44 +0000

    add --convert-links

--- bin/wget.sh
+++ bin/wget.sh
@@ -18,7 +18,7 @@ dstdir=${topdir}/wget
 mkdir -p ${dstdir}
 mkdir -p ${logdir}
 
-time ${wget} --directory-prefix=${dstdir}/mirror --recursive --page-requisites --server-response --timestamping --level ${level} ${URL}
+time ${wget} --directory-prefix=${dstdir}/mirror --recursive --page-requisites --convert-links --server-response --timestamping --level ${level} ${URL}
 ## -r : --recursive
 ## -k : --convert-links
 ## -np : --no-parent
