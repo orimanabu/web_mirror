c795afcbe8b719549b3d24e3384a98c77fe08c4a
Manabu Ori <orim@jp.ibm.com>
2011-03-20 07:58:50 +0000

    uncomment MacOSX settings.

--- env.sh
+++ env.sh
@@ -1,7 +1,11 @@
-site_docroot=${HOME}/Sites/web_mirror
+# site_docroot: Apache Document Root (default: /var/www/html)
+# site_docroot=${HOME}/Sites/web_mirror
 
-site_wget=/opt/local/bin/wget
-site_pavuk=/opt/local/bin/pavuk
+# site_wget: path of wget (default: /usr/local/bin/wget)
+# site_wget=/opt/local/bin/wget
+
+# site_pavuk: path of pavuk (default: /usr/local/bin/pavuk)
+# site_pavuk=/opt/local/bin/pavuk
 
 #site_fetcher=pavuk
 site_fetcher=wget
